51f3d2d7dfeabb2c023221d766f777357ce5504d
bthavaselvi <ocr.force2024@gmail.com>
2024-01-22 22:04:56 -0700

    saving working copy

--- force-app/main/default/lwc/oCRComponent/oCRComponent.js
+++ force-app/main/default/lwc/oCRComponent/oCRComponent.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import uploadFile from '@salesforce/apex/FileUploaderClass.uploadFile'
 export default class FileUploaderCompLwc extends LightningElement {
-    @api recordId;
+  
     fileData
     openfileUpload(event) {
         const file = event.target.files[0]
@@ -11,20 +11,19 @@ export default class FileUploaderCompLwc extends LightningElement {
             var base64 = reader.result.split(',')[1]
             this.fileData = {
                 'filename': file.name,
-                'base64': base64,
-                'recordId': this.recordId
+                'base64': base64
             }
-            console.log(this.fileData)
+            
         }
         reader.readAsDataURL(file)
     }
     
     handleClick(){
-        const {base64, filename, recordId} = this.fileData
-        uploadFile({ base64, filename, recordId }).then(result=>{
+        const {base64, filename} = this.fileData
+        uploadFile({ base64, filename }).then(result=>{
             this.fileData = null
             let title = `${filename} uploaded successfully!!`
-            this.toast(title)
+            this.toast(result)
         })
     }
 
